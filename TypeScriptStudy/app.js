e019446ad74f1ae1863009a081d814bab6003bda
kimsejune <tpwns1088@gmail.com>
2021-12-18 12:05:30 +0900

    TypeScript Object Type 설정시
    Object의 Property의 값이 할당되어있다면 컴파일러가 추론을 해줄테니
    
    const person: {
      name: string;
      age: number;
    } = {
      name: "Maximilian",
      age: 30,
    };
    위의 코드처럼 작성할 필요 없이 아래처럼 작성하여 추론하도록 한다.
    
    const person = {
      name: "Maximilian",
      age: 30,
    };

--- TypeScriptStudy/app.js
+++ TypeScriptStudy/app.js
@@ -1,28 +1,12 @@
-function add(n1, n2, showResult, phrase) {
-    // 단점 runtime에서만 체크할 수 있다. (TypeScript 사용시 아래와같은 방법은 사용하지 않는 것이 좋다.)
-    // javascript는 number, string, boolean Type만 알 수 있다.
-    // typescript를 통하여 런타임이아닌 코드작성중에 방어할 수 있다.
-    // if (typeof n1 !== "number" || typeof n2 !== "number") {
-    //   throw new Error("Incorrect input!!");
-    // }
-    // 상수 result는 타입추론을 통하여 타입을 추론한다.
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-// let number1은 typescript가 number type이라고 추론이 가능하기때문에
-// 이미 할당되어있는 값을 기반으로 추론을 하기때문이다.
-// 굳이 명시하는것이 좋지않다.
-// let number1 = 5;
-var number1 = 5; // JS에서 5는 5.0과 같다.
-// if (5 === 5.0) {
-//   console.log("same");
+var person = {
+    name: "Maximilian",
+    age: 30
+};
+// 없는 property를 찾으려고하면 오류가난다.
+// person: object로 설정하면 person의 구체적 property를 알지 못하여 존재하는 property도 알아내지 못한다.
+console.log(person.name);
+// 아래와 같이 타입이 보여진다.
+// const person: {
+//   name: string;
+//   age: number;
 // }
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result is: ";
-add(number1, number2, printResult, resultPhrase);
