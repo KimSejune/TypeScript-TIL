242fd2dac5ee07d79291f307b87117ca5034a4fd
kimsejune <tpwns1088@gmail.com>
2021-12-19 15:37:24 +0900

    array type 추가

--- TypeScriptStudy/app.js
+++ TypeScriptStudy/app.js
@@ -1,10 +1,20 @@
+// const person: {
+//   name: string;
+//   age: number;
+// }
 var person = {
     name: "Maximilian",
-    age: 30
+    age: 30,
+    hobbies: ["Sports", "cooking"]
 };
+var favoriteAcitivities = ["Sports"];
 // 없는 property를 찾으려고하면 오류가난다.
 // person: object로 설정하면 person의 구체적 property를 알지 못하여 존재하는 property도 알아내지 못한다.
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
 // 아래와 같이 타입이 보여진다.
 // const person: {
 //   name: string;
--- TypeScriptStudy/app.ts
+++ TypeScriptStudy/app.ts
@@ -5,14 +5,11 @@
 const person = {
   name: "Maximilian",
   age: 30,
+  hobbies: ["Sports", "cooking"],
 };
 
-// 없는 property를 찾으려고하면 오류가난다.
-// person: object로 설정하면 person의 구체적 property를 알지 못하여 존재하는 property도 알아내지 못한다.
-console.log(person.name);
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
 
-// 아래와 같이 타입이 보여진다.
-// const person: {
-//   name: string;
-//   age: number;
-// }
+let favoriteAcitivities: string[] = ["Sports"];
