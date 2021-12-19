b541919b73d34456fb9b850b0e975c5add994fc6
kimsejune <tpwns1088@gmail.com>
2021-12-19 15:48:51 +0900

    tuple type

--- TypeScriptStudy/app.js
+++ TypeScriptStudy/app.js
@@ -5,18 +5,14 @@
 var person = {
     name: "Maximilian",
     age: 30,
-    hobbies: ["Sports", "cooking"]
+    hobbies: ["Sports", "cooking"],
+    role: [2, "author"]
 };
-var favoriteAcitivities = ["Sports"];
-// 없는 property를 찾으려고하면 오류가난다.
-// person: object로 설정하면 person의 구체적 property를 알지 못하여 존재하는 property도 알아내지 못한다.
-console.log(person.name);
+person.role.push("admin");
+// person.role[1] = 10;
+console.log(person.role);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby);
+    console.log(hobby.toUpperCase());
 }
-// 아래와 같이 타입이 보여진다.
-// const person: {
-//   name: string;
-//   age: number;
-// }
+var favoriteAcitivities = ["Sports"];
--- TypeScriptStudy/app.ts
+++ TypeScriptStudy/app.ts
@@ -2,12 +2,23 @@
 //   name: string;
 //   age: number;
 // }
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Maximilian",
   age: 30,
   hobbies: ["Sports", "cooking"],
+  role: [2, "author"],
 };
 
+// person.role.push("admin");
+
+// person.role = [1, "test", 3];
+
+console.log(person.role);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
 }
