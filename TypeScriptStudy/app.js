dccb1e3f57bcbbac74087f82d03d773d967cca8b
kimsejune <tpwns1088@gmail.com>
2021-12-20 20:40:51 +0900

    function return type

--- TypeScriptStudy/app.js
+++ TypeScriptStudy/app.js
@@ -1,24 +1,7 @@
 function add(n1, n2) {
-    var result = n1 + n2;
-    return result;
+    return n1 + n2;
 }
-function combine(input1, input2, resultConversation) {
-    var result;
-    if ((typeof input1 === "number" && typeof input2 === "number") ||
-        resultConversation === "as-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversation === "as-number") {
-    //   return parseFloat(result);
-    // } else {
-    //   return result.toString();
-    // }
+function printResult(num) {
+    console.log("Result : " + num);
 }
-var combinedAges = combine(30, 26, "as-number");
-var combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-var combinedNames = combine("Max", "Kim", "as-text");
+printResult(add(5, 12));
--- TypeScriptStudy/app.ts
+++ TypeScriptStudy/app.ts
@@ -1,36 +1,9 @@
 function add(n1: number, n2: number) {
-  const result = n1 + n2;
-  return result;
+  return n1 + n2;
 }
 
-type Combinable = number | string;
-type ConversionDescriptor = "as-number" | "as-text";
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversation: ConversionDescriptor
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversation === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-
-  // if (resultConversation === "as-number") {
-  //   return parseFloat(result);
-  // } else {
-  //   return result.toString();
-  // }
+function printResult(num: number): void {
+  console.log("Result : " + num);
 }
-const combinedAges = combine(30, 26, "as-number");
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-
-const combinedNames = combine("Max", "Kim", "as-text");
+printResult(add(5, 12));
