f509504907e39e0ee71497bb19c939b25648517e
kimsejune <tpwns1088@gmail.com>
2021-12-26 10:50:51 +0900

    Add watch mode

--- TypeScriptStudy/app.js
+++ TypeScriptStudy/app.js
@@ -1,14 +1,3 @@
-var userInput; // 사용자가 어떤 값을 입력할지 몰라서 unknown이다.
-var userName;
-userInput = 5;
-userInput = "Max";
-if (typeof userInput === "string") {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-console.log("Before");
-var result = generateError("An error occurred!", 500);
-console.log("After");
-console.log(result);
+var watch = "watch me";
+// tsc app.ts --watch
+console.log(watch);
--- TypeScriptStudy/app.ts
+++ TypeScriptStudy/app.ts
@@ -1,18 +1,3 @@
-let userInput: unknown; // 사용자가 어떤 값을 입력할지 몰라서 unknown이다.
-let userName: string;
-
-userInput = 5;
-userInput = "Max";
-
-if (typeof userInput === "string") {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
-
-console.log("Before");
-const result = generateError("An error occurred!", 500);
-console.log("After");
-console.log(result);
+const watch = "watch me";
+// tsc app.ts --watch
+console.log(watch);
